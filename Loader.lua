714422799d3aaba203b34d83a5aa40a36bdb497e
putraborz <putra.myapi@gmail.com>
2025-10-31 22:33:21 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/putraborz/LxMtKomang/refs/heads/main/main.lua"
+local url = "https://raw.githubusercontent.com/putraborz/LxMtKomang/main/Loader/main.lua"
 local s, e = pcall(function()
     return loadstring(game:HttpGet(url))()
 end)
